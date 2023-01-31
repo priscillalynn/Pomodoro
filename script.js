941df465c8fbb45c329be27d57ca2f62c98e838b
Priscilla Colon <plynn9036@gmail.com>
2023-01-30 20:44:28 -0500

    need to fix timer

---
+++ script.js
@@ -0,0 +1,66 @@
+let workTitle = document.getElementById('work');
+let breakTitle = document.getElementById('break');
+
+let workTime = 1;
+let breakTime = 5;
+
+let seconds = "00";
+
+//display
+window.onload = () => {
+    document.getElementById('minutes').innerHTML = workTime;
+    document.getElementById('seconds').innerHTML = seconds;
+
+    workTitle.classList.add('active');
+}
+
+//start timer
+function start() {
+    //change button
+    document.getElementById('start').style.display = "none";
+    document.getElementById('reset').style.display = "block";
+
+    // change the time
+    seconds = 3;
+
+    let workMins = workTime - 1;
+    let breakMins = breakTime - 1;
+
+    breakCount = 0;
+
+    //countdown
+    let timerFunction = () => {
+        //change the display
+        document.getElementById('minutes').innerHTML = workMins;
+        document.getElementById('seconds').innerHTML = seconds;
+
+        //start
+        seconds = seconds - 1;
+
+        if(seconds === 0) {
+            workMins = workMins - 1;
+            if(workMins === -1) {
+                if(breakCount % 2 === 0) {
+                    // start break
+                    workMins = breakMins;
+                    breakCount++;
+                    //change panel
+                    workTitle.classList.remove('active');
+                    breakTitle.classList.add('active');
+                } else {
+                    //continue work
+                    workMins = workTime;
+                    breakCount++;
+
+                    //change panel
+                    breakTitle.classList.remove('active');
+                    workTitle.classList.add('active');
+                }
+            }
+            seconds = 59;
+        }
+    }
+
+    //start countdown
+    setInterval(timerFunction, 1000); //1000 = 1s
+}
